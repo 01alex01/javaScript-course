2e42f4c23885db39c0b10607ae92720ad9d9d874
alex atolagbe <01atolagbealex01@gmail.com>
2024-01-13 15:52:46 +0100

    firstCommit

---
+++ script.js
@@ -0,0 +1,11 @@
+let js = "amazing"; //semicolon
+
+console.log(40 + 8 - 10);
+console.log("jonas");
+console.log(23);
+
+let firstName = "maltilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
