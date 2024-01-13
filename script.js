8a46160de51aff269adf1fc71293a04cc3afe35d
alex atolagbe <01atolagbealex01@gmail.com>
2024-01-13 17:04:49 +0100

    the 7 primitive DATA  TYPE

--- script.js
+++ script.js
@@ -19,3 +19,12 @@ let PI = 3.1415;
 let myFirstJob = "coder";
 let myCurrentJob = "teacher";
 console.log(myFirstJob);
+//the 7 primitive DATA TYPE
+// so there are diffrent type of primitive number,string,bullion,undefined,null,symbol and big int.
+// Number are always so called used for decimal and integers
+// string is a sequence of characters Used for text
+// Boolean logical type that can only be true or false used for taking decisions
+//undefined:value taken by a variable that is not yet defined ('empy value')
+//null also means 'empty value'
+//symbol (ES2015):value that is unique and cannot be change [not useful for now]
+//Biglnt (ES2020): Larger integers than the number type can hold
