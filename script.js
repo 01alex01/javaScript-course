a89263212dd8bb89bed18b3d91c74283ec9e8cf4
alex atolagbe <01atolagbealex01@gmail.com>
2024-01-13 16:42:57 +0100

    working on camelcase

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ let firstName = "maltilda";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+let jonas_matilda = "jm";
+let $finction = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "coder";
+let myCurrentJob = "teacher";
+console.log(myFirstJob);
