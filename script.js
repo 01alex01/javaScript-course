93cf8dfac37f1b22b37bf44feeb980d3b7c6c1c4
alex atolagbe <01atolagbealex01@gmail.com>
2024-01-14 17:24:16 +0100

    comment out all code line

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ let PI = 3.1415;
 let myFirstJob = "coder";
 let myCurrentJob = "teacher";
 console.log(myFirstJob);
+
 //the 7 primitive DATA TYPE
 // so there are diffrent type of primitive number,string,bullion,undefined,null,symbol and big int.
 // Number are always so called used for decimal and integers
