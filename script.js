2d17b91ec2dfe3c045f240720d4ffc3de06907ef
alex atolagbe <01atolagbealex01@gmail.com>
2024-01-14 21:53:51 +0100

    new commit

--- script.js
+++ script.js
@@ -70,3 +70,21 @@ var job = "programmer";
 job = "teacher";
 console.log(job);
 */
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+//2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "jonas";
+const lastName = "Schmedtman";
+console.log(firstName + " " + lastName);
+
+//assingment operator
+let x = 10 + 5; //15
+//reassingning the x value
+x += 10; //x = x + 10 = 25
+x *= 4; //x=x * 4 = 100
+console.log(x);
