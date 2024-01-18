29c61c3f8f0537d89f5d5b07def9be0cdd515815
alex atolagbe <01atolagbealex01@gmail.com>
2024-01-18 13:38:00 +0100

    taking decision: if else statement

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ console.log(x, y);
 
 const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);
-*/
+
 const firstName = "jonas";
 const job = "teacher";
 const birthYear = 1991;
@@ -141,3 +141,23 @@ console.log(`string
 multiple
 lines`);
 //backticks ``
+*/
+const age = 15;
+
+if (age >= 18) {
+  console.log("sarah can start driving license");
+} else {
+  const yearLeft = 18 - age;
+  console.log(`sarah is too young. wait another ${yearLeft} years :`);
+}
+
+const birthYear = 2012;
+
+let century;
+
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
